7c9f72ccebdc24b15e3bd6036b5081a1c454321d
razabangi <muhammadrazabangi9@gmail.com>
2024-05-08 00:33:04 +0500

    linear search | find min

--- linear-search.js
+++ linear-search.js
@@ -1,4 +1,4 @@
-let arr = [5,3,6,2,1,7,9,0,21,26,4];
+let arrForFindMax = [5,3,6,2,1,7,9,0,21,26,4];
 
 function findMax(arr) {
   let max = arr[0];
@@ -12,4 +12,20 @@ function findMax(arr) {
   return max;
 }
 
-console.log(findMax(arr));
+console.log(findMax(arrForFindMax));
+
+let arrForFindMin = [5,3,6,80,30,7,9,26,21,26,4];
+
+function findMin(arr) {
+  let min = arr[0];
+
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  
+  return min;
+}
+
+console.log(findMin(arrForFindMin));
