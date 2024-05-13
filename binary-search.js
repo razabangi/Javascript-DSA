60f9f460138777220ad2926e478e20e5e9676948
razabangi <muhammadrazabangi9@gmail.com>
2024-05-14 03:31:27 +0500

    getting ceil value by the help of binary search

--- binary-search.js
+++ binary-search.js
@@ -21,3 +21,30 @@ function binarySearch(arr, target) {
 }
 
 console.log(binarySearch(arr, target));
+
+// -----------------------------------------------------------------------
+// Get Ceil Value
+
+let arrayForGettingCeil = [1,4,5,6,7,8,10,15,16,18,20];
+let targetValue = 17;
+
+function getCeilValue(arr, target) {
+  let start = arr[0];
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    let mid = start + Math.floor((end - start) / 2);
+
+    if (target > arr[mid]) {
+      start = mid + 1;
+    } else if (target < arr[mid]) {
+      end = mid - 1
+    } else {
+      return mid;
+    }
+  }
+
+  return arr[start];
+}
+
+console.log(getCeilValue(arr, target));
