f1429d3dcd730af4ba84ee794ef249b7aec98b25
razabangi <muhammadrazabangi9@gmail.com>
2024-05-18 01:57:34 +0500

    factorial by using recursion

--- recursion.js
+++ recursion.js
@@ -7,4 +7,18 @@ function recursion(num) {
   
   let num = 5;
   recursion(num); // 5,4,3,2,1
+
+  // ========================== factorial by using resursion
+
+  let target = 5;
+
+  function factorial(num) {
+    if (num === 0) {
+      return 1;
+    }
+  
+    return num * factorial(num - 1)
+  }
+  
+  console.log(factorial(target))
   
